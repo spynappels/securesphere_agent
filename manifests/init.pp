900d02e53cbbfcb66b8b53d7bf9f15cd2420f160
Stefan Pynappels <stefan.pynappels@imperva.com>
2018-12-18 14:10:16 +0000

    Confirmed as working on a Windows2008 server, now for the rest...

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,19 @@
-class aioinstall {
+class aioinstall (
+  String $installer_loc = 'C:\Users\Public\Downloads\Imperva-ragent-Windows-b11.0.0.8026',
+  String $installer_file = 'Imperva-ragent-Windows-b11.0.0.8026.msi',
+  String $install_dir = 'C:\Program Files (x86)\Imperva',
+  String $agent_name = 'puppet_ssagent',
+  String $gw_ip = '10.4.253.103'
+  ){
   package { 'Imperva SecureSphere Remote Agent':
     ensure => installed,
-    source => 'C:\Users\Public\Downloads\Imperva-ragent-Windows-b11.0.0.8026\Imperva-ragent-Windows-b11.0.0.8026.msi',
-    install_options => [ { 'TARGETDIR' => 'C:\Program Files (x86)\Imperva'}, { 'NOSCRIPT' => 'true'} ],
+    source => $installerloc$installer_file,
+    install_options => [ { 'TARGETDIR' => $install_dir}, { 'NOSCRIPT' => 'true'} ],
   }
 
   exec { 'register-ragent':
     path => 'C:/Program Files (x86)/Imperva/RemoteAgent',
-    command => 'RemoteAgentCli.exe registration advanced-register is-db-agent=true ragent-name=pupp-ragent gateway=10.4.253.103 password=secure'
+    command => "RemoteAgentCli.exe registration advanced-register is-db-agent=true ragent-name=$agent_name gateway=$gwip password=secure"
     subscribe => Package['Imperva SecureSphere Remote Agent'],
     refreshonly => true,
 	  before => Service['SecureSphereRemoteAgent'],
